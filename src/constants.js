ededf792644b3e685cf807c83475c75527776645
VentOSi <98553334+VentOSi@users.noreply.github.com>
2023-10-15 21:26:11 +0300

    4-th step. Handling RSS

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const FORM_STATE = {
+  empty: "empty",
+  typing: "typing",
+  sending: "sending",
+  error: "error",
+  success: "success",
+};
---
+++ src/validateUrl.js
@@ -0,0 +1,13 @@
+import { string } from "yup";
+import i18next from "i18next";
+
+const validateFeedUrl = (value, feeds) =>
+  string()
+    .test("uniq", i18next.t("validationErrorIsset"), function () {
+      return !feeds.includes(value) || this.createError();
+    })
+    .url(i18next.t("validationErrorUrl"))
+    .required()
+    .validate(value);
+
+export default validateFeedUrl;
