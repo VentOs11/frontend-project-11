5cc2d5228645a016698140b40ce076623b71b5cf
VentOSi <98553334+VentOSi@users.noreply.github.com>
2023-10-16 22:30:55 +0300

    fix lint

--- src/text.js
+++ src/text.js
@@ -2,7 +2,7 @@ export default {
   ru: {
     translation: {
       validationErrorIsset: "RSS уже существует",
-      validationErrorUrl: "Ресурс не содержит валидный RSS",
+      validationErrorUrl: "Ссылка должна быть валидным URL",
       networkError: "Ошибка сети",
       parseError: "Ошибка чтения данных",
       successMessage: "RSS успешно загружен",
