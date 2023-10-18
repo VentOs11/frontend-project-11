4c0d3054046a1cc5293710392598609fa2b4f31d
VentOSi <98553334+VentOSi@users.noreply.github.com>
2023-10-18 23:12:12 +0300

    fix linter again

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 export const FORM_STATE = {
-  empty: "empty",
-  typing: "typing",
-  sending: "sending",
-  error: "error",
-  success: "success",
+  "empty": "empty",
+  "typing": "typing",
+  "sending": "sending",
+  "error": "error",
+  "success": "success",
 };
 
 export const UPDATE_TIME = 5000;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import startApp from './startApp';
-
 startApp();
