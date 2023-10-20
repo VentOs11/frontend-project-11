a83d733604d05f46b8e7b48b780a7da01709eb03
VentOSi <98553334+VentOSi@users.noreply.github.com>
2023-10-20 21:05:17 +0300

    fixing

--- src/validateUrl.js
+++ src/validateUrl.js
@@ -1,12 +1,11 @@
-import { string } from "yup";
-import i18next from "i18next";
+import { string } from 'yup';
+import i18next from 'i18next';
 
-const validateFeedUrl = (value, feeds) =>
-  string()
-    .test("uniq", i18next.t("validationErrorIsset"), function uniq() {
+const validateFeedUrl = (value, feeds) => string()
+    .test('uniq', i18next.t('validationErrorIsset'), function uniq() {
       return !feeds.includes(value) || this.createError();
     })
-    .url(i18next.t("validationErrorUrl"))
+    .url(i18next.t('validationErrorUrl'))
     .required()
     .validate(value);
 
