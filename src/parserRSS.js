84165aa07fa6104c246ca970b93c93df8d59fb3e
VentOSi <98553334+VentOSi@users.noreply.github.com>
2023-10-20 21:13:05 +0300

    hope last fix linter

--- src/parserRSS.js
+++ src/parserRSS.js
@@ -6,13 +6,11 @@ const parseRss = (data) => {
   const title = channel.getElementsByTagName('title')[0].textContent;
   const description = channel.getElementsByTagName('description')[0].textContent;
   const posts = [...channel.getElementsByTagName('item')].map((i) => ({
-    'title': parseString(i.getElementsByTagName('title')[0].textContent),
-    'description': parseString(
-      i.getElementsByTagName('description')[0].textContent
-    ),
-    'link': i.getElementsByTagName('link')[0].textContent,
-    'timestamp': new Date(i.getElementsByTagName('pubDate')[0].textContent).valueOf(),
-    'isRead': false,
+    title: parseString(i.getElementsByTagName('title')[0].textContent),
+    description: parseString(i.getElementsByTagName('description')[0].textContent),
+    link: i.getElementsByTagName('link')[0].textContent,
+    timestamp: new Date(i.getElementsByTagName('pubDate')[0].textContent).valueOf(),
+    isRead: false,
   }));
 
   return { title, description, posts };
--- src/text.js
+++ src/text.js
@@ -1,12 +1,12 @@
 export default {
   ru: {
     translation: {
-      'validationErrorIsset': 'RSS уже существует',
-      'validationErrorUrl': 'Ссылка должна быть валидным URL',
-      'networkError': 'Ошибка сети',
-      'parseError': 'Ресурс не содержит валидный RSS',
-      'successMessage': 'RSS успешно загружен',
-      'buttonText': 'Просмотр',
+      validationErrorIsset: 'RSS уже существует',
+      validationErrorUrl: 'Ссылка должна быть валидным URL',
+      networkError: 'Ошибка сети',
+      parseError: 'Ресурс не содержит валидный RSS',
+      successMessage: 'RSS успешно загружен',
+      buttonText: 'Просмотр',
     },
   },
 };
