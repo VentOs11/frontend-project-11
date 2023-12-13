6584fa994ee95e58bbacff7a2b9a7afb20cd6610
VentOSi <98553334+VentOSi@users.noreply.github.com>
2023-12-13 23:35:42 +0300

    ff

--- src/render.js
+++ src/render.js
@@ -1,14 +1,14 @@
 const createFeeds = (state) => {
   const feeds = [];
   state.feeds.forEach((feed) => {
-    const liEl = document.createElement("li");
-    liEl.classList.add("list-group-item", "border-0", "border-end-0");
-    const feedTitle = document.createElement("h3");
-    feedTitle.classList.add("h6", "m-0");
+    const liEl = document.createElement('li');
+    liEl.classList.add('list-group-item', 'border-0', 'border-end-0');
+    const feedTitle = document.createElement('h3');
+    feedTitle.classList.add('h6', 'm-0');
     feedTitle.textContent = feed.title;
     liEl.append(feedTitle);
-    const pEl = document.createElement("p");
-    pEl.classList.add("m-0", "small", "text-black-50");
+    const pEl = document.createElement('p');
+    pEl.classList.add('m-0', 'small', 'text-black-50');
     pEl.textContent = feed.description;
     liEl.append(pEl);
     feeds.push(liEl);
@@ -17,37 +17,37 @@ const createFeeds = (state) => {
 };
 
 const createButton = (post, i18next) => {
-  const buttonEl = document.createElement("button");
-  buttonEl.setAttribute("type", "button");
-  buttonEl.setAttribute("data-id", post.id);
-  buttonEl.setAttribute("data-bs-toggle", "modal");
-  buttonEl.setAttribute("data-bs-target", "#modal");
-  buttonEl.classList.add("btn", "btn-outline-primary", "btn-sm");
-  buttonEl.textContent = i18next.t("buttons.view");
+  const buttonEl = document.createElement('button');
+  buttonEl.setAttribute('type', 'button');
+  buttonEl.setAttribute('data-id', post.id);
+  buttonEl.setAttribute('data-bs-toggle', 'modal');
+  buttonEl.setAttribute('data-bs-target', '#modal');
+  buttonEl.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+  buttonEl.textContent = i18next.t('buttons.view');
   return buttonEl;
 };
 
 const createPosts = (state, i18next) => {
   const posts = [];
   state.posts.forEach((post) => {
-    const liEl = document.createElement("li");
+    const liEl = document.createElement('li');
     liEl.classList.add(
-      "list-group-item",
-      "d-flex",
-      "justify-content-between",
-      "align-items-start",
-      "border-0",
-      "border-end-0"
+      'list-group-item',
+      'd-flex',
+      'justify-content-between',
+      'align-items-start',
+      'border-0',
+      'border-end-0'
     );
-    const aEl = document.createElement("a");
-    aEl.setAttribute("href", post.link);
-    aEl.setAttribute("data-id", post.id);
-    aEl.setAttribute("target", "_blank");
-    aEl.setAttribute("rel", "noopener noreferrer");
+    const aEl = document.createElement('a');
+    aEl.setAttribute('href', post.link);
+    aEl.setAttribute('data-id', post.id);
+    aEl.setAttribute('target', '_blank');
+    aEl.setAttribute('rel', 'noopener noreferrer');
     if (state.uiState.viewedPostIds.has(post.id)) {
-      aEl.classList.add("fw-normal");
+      aEl.classList.add('fw-normal');
     } else {
-      aEl.classList.add("fw-bold");
+      aEl.classList.add('fw-bold');
     }
     aEl.textContent = post.title;
     const buttonEl = createButton(post, i18next);
@@ -59,22 +59,22 @@ const createPosts = (state, i18next) => {
 };
 
 const createList = (itemsType, state, i18next) => {
-  const card = document.createElement("div");
-  card.classList.add("card", "border-0");
-  const cardBody = document.createElement("div");
-  cardBody.classList.add("card-body");
-  const cardTitle = document.createElement("h2");
-  cardTitle.classList.add("card-title", "h4");
-  const list = document.createElement("ul");
-  list.classList.add("list-group", "border-0", "rounded-0");
+  const card = document.createElement('div');
+  card.classList.add('card', 'border-0');
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title', 'h4');
+  const list = document.createElement('ul');
+  list.classList.add('list-group', 'border-0', 'rounded-0');
   cardBody.append(cardTitle);
   card.append(cardBody);
   cardTitle.textContent = i18next.t(`items.${itemsType}`);
   switch (itemsType) {
-    case "feeds":
+    case 'feeds':
       list.append(...createFeeds(state));
       break;
-    case "posts":
+    case 'posts':
       list.append(...createPosts(state, i18next));
       break;
     default:
@@ -86,41 +86,43 @@ const createList = (itemsType, state, i18next) => {
 
 const renderInvalid = ({ submit, urlInput, feedback }) => {
   submit.disabled = false;
-  urlInput.classList.add("is-invalid");
-  feedback.classList.remove("text-success");
-  feedback.classList.remove("text-warning");
-  feedback.classList.add("text-danger");
+  urlInput.classList.add('is-invalid');
+  feedback.classList.remove('text-success');
+  feedback.classList.remove('text-warning');
+  feedback.classList.add('text-danger');
 };
 
 const renderSending = ({ submit, urlInput, feedback }, i18next) => {
   submit.disabled = true;
-  urlInput.classList.remove("is-invalid");
-  feedback.classList.remove("text-danger");
-  feedback.classList.remove("text-success");
-  feedback.classList.add("text-warning");
-  feedback.textContent = i18next.t("status.sending");
+  urlInput.classList.remove('is-invalid');
+  feedback.classList.remove('text-danger');
+  feedback.classList.remove('text-success');
+  feedback.classList.add('text-warning');
+  feedback.textContent = i18next.t('status.sending');
 };
 
-const renderAdded = ({ submit, urlInput, feedback, form }, i18next) => {
+const renderAdded = (
+  { submit, urlInput, feedback, form },
+  i18next) => {
   submit.disabled = false;
-  urlInput.classList.remove("is-invalid");
-  feedback.classList.remove("text-danger");
-  feedback.classList.remove("text-warning");
-  feedback.classList.add("text-success");
-  feedback.textContent = i18next.t("status.success");
+  urlInput.classList.remove('is-invalid');
+  feedback.classList.remove('text-danger');
+  feedback.classList.remove('text-warning');
+  feedback.classList.add('text-success');
+  feedback.textContent = i18next.t('status.success');
   form.reset();
   urlInput.focus();
 };
 
 const renderState = (elements, i18next, value) => {
   switch (value) {
-    case "invalid":
+    case 'invalid':
       renderInvalid(elements);
       break;
-    case "sending":
+    case 'sending':
       renderSending(elements, i18next);
       break;
-    case "added": {
+    case 'added': {
       renderAdded(elements, i18next);
       break;
     }
@@ -134,19 +136,19 @@ const renderError = (state, { feedback }, i18next, error) => {
     return;
   }
 
-  feedback.classList.add("text-danger");
+  feedback.classList.add('text-danger');
   feedback.textContent = i18next.t(`errors.${state.error}`);
 };
 
 const renderFeeds = (state, { feedsList }, i18next) => {
-  feedsList.innerHTML = "";
-  const feeds = createList("feeds", state, i18next);
+  feedsList.innerHTML = '';
+  const feeds = createList('feeds', state, i18next);
   feedsList.append(feeds);
 };
 
 const renderPosts = (state, { postsList }, i18next) => {
-  postsList.innerHTML = "";
-  const posts = createList("posts", state, i18next);
+  postsList.innerHTML = '';
+  const posts = createList('posts', state, i18next);
   postsList.append(posts);
 };
 
@@ -159,25 +161,25 @@ const renderDisplayedPost = (
   const [{ description, link, title }] = posts;
   modalHeader.textContent = title;
   modalBody.textContent = description;
-  modalHref.setAttribute("href", link);
+  modalHref.setAttribute('href', link);
 };
 
 const render = (state, elements, i18next) => (path, value) => {
   switch (path) {
-    case "formState":
+    case 'formState':
       renderState(elements, i18next, value);
       break;
-    case "error":
+    case 'error':
       renderError(state, elements, i18next, value);
       break;
-    case "feeds":
+    case 'feeds':
       renderFeeds(state, elements, i18next);
       break;
-    case "posts":
-    case "uiState.viewedPostIds":
+    case 'posts':
+    case 'uiState.viewedPostIds':
       renderPosts(state, elements, i18next);
       break;
-    case "uiState.displayedPost":
+    case 'uiState.displayedPost':
       renderDisplayedPost(state, elements, value);
       break;
     default:
--- src/rss.js
+++ src/rss.js
@@ -1,8 +1,8 @@
 const parsePost = (post) => {
-  const link = post.querySelector("link").textContent;
-  const title = post.querySelector("title").textContent;
-  const description = post.querySelector("description").textContent;
-  const date = post.querySelector("pubDate").textContent;
+  const link = post.querySelector('link').textContent;
+  const title = post.querySelector('title').textContent;
+  const description = post.querySelector('description').textContent;
+  const date = post.querySelector('pubDate').textContent;
   return {
     link,
     title,
@@ -13,23 +13,23 @@ const parsePost = (post) => {
 
 const parse = (rss, url) => {
   const parser = new DOMParser();
-  const data = parser.parseFromString(rss, "text/xml");
-  const parseError = data.querySelector("parsererror");
+  const data = parser.parseFromString(rss, 'text/xml');
+  const parseError = data.querySelector('parsererror');
   if (parseError) {
     const error = new Error(parseError.textContent);
     error.isParsingError = true;
     throw error;
   }
 
-  const feedTitle = data.querySelector("title").textContent;
-  const feedDescription = data.querySelector("description").textContent;
+  const feedTitle = data.querySelector('title').textContent;
+  const feedDescription = data.querySelector('description').textContent;
   const feed = {
     link: url,
     title: feedTitle,
     description: feedDescription,
   };
 
-  const posts = [...data.querySelectorAll("item")].map(parsePost);
+  const posts = [...data.querySelectorAll('item')].map(parsePost);
   return { feed, posts };
 };
 
